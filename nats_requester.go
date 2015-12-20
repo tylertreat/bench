79953a2ee52bedf0c4292f754a829a93674dee31
Tyler Treat <ttreat31@gmail.com>
2015-12-20 02:14:19 -0600

    Implement NATSRequester

---
+++ nats_requester.go
@@ -0,0 +1,55 @@
+package bench
+
+import (
+	"time"
+
+	"github.com/nats-io/nats"
+)
+
+// NATSRequester implements Requester by publishing a message to NATS and
+// waiting to receive it.
+type NATSRequester struct {
+	URL         string
+	PayloadSize int
+	Subject     string
+	conn        *nats.Conn
+	sub         *nats.Subscription
+	msg         []byte
+}
+
+// Setup prepares the Requester for benchmarking.
+func (n *NATSRequester) Setup() error {
+	conn, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		return err
+	}
+	sub, err := conn.SubscribeSync(n.Subject)
+	if err != nil {
+		return err
+	}
+	n.conn = conn
+	n.sub = sub
+	n.msg = make([]byte, n.PayloadSize)
+	return nil
+}
+
+// Request performs a synchronous request to the system under test.
+func (n *NATSRequester) Request() error {
+	if err := n.conn.Publish(n.Subject, n.msg); err != nil {
+		return err
+	}
+	_, err := n.sub.NextMsg(5 * time.Second)
+	return err
+}
+
+// Teardown is called upon benchmark completion.
+func (n *NATSRequester) Teardown() error {
+	err := n.sub.Unsubscribe()
+	if err != nil {
+		return err
+	}
+	n.sub = nil
+	n.conn.Close()
+	n.conn = nil
+	return nil
+}
